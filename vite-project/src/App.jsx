d5830d3722b7a73b230b86917bd513b3bb608ad5
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-05-30 16:45:59 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Hero from './components/Hero'
 import Villains from './components/Villains'
+import Mission from './components/Mission'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <main>
       <Hero />
       <Villains />
+      <Mission />
     </main>
   )
 }
